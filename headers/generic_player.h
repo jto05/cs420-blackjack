b80a13d8a9b12e50ace0b5341a551b6a5291dc4c
jto05 <julian.to2023@gmail.com>
2025-11-06 18:12:45 -0800

    Added implementation of GenericPlayer class; missing documentation and testing

--- headers/generic_player.h
+++ headers/generic_player.h
@@ -1,4 +1,23 @@
 #ifndef GENERIC_PLAYER_H
 #define GENERIC_PLAYER_H
 
+#include "../headers/hand.h"
+#include <iostream>
+
+class GenericPlayer : public Hand {
+private:
+  std::string name;
+
+public:
+  GenericPlayer(std::string name);
+  virtual ~GenericPlayer();
+
+  bool virtual isHitting();
+  bool isBusted();
+  bool bust();
+  std::string getName();
+  void setName(std::string name);
+
+};
+
 #endif
---
+++ source/generic_player.cpp
@@ -0,0 +1,34 @@
+#include "../headers/generic_player.h"
+
+#include <iostream>
+#include <cstdio>
+
+GenericPlayer::GenericPlayer(std::string name) {
+  this->name = name;
+}
+
+GenericPlayer::~GenericPlayer() {
+}
+
+bool GenericPlayer::isHitting() {
+  return false;
+}
+
+
+bool GenericPlayer::isBusted() {
+  int total = getTotal();
+  return ( total < 21 );
+}
+
+bool GenericPlayer::bust() {
+  std::printf( "%s has busted!!!!!!\n", name.c_str() );
+  return isBusted();
+};
+
+std::string GenericPlayer::getName() {
+  return name;
+}
+
+void GenericPlayer::setName(std::string name) {
+  this->name = name;
+}
