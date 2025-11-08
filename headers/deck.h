a05493355b0bd933af0f80ed922dc462270540ce
jto05 <julian.to2023@gmail.com>
2025-11-07 16:37:32 -0800

    Implemented Deck class; requires testing still

--- headers/deck.h
+++ headers/deck.h
@@ -1,4 +1,18 @@
 #ifndef DECK_H
 #define DECK_H
 
+#include "../headers/hand.h"
+#include "../headers/generic_player.h"
+
+
+class Deck : public Hand {
+public:
+  Deck();
+  virtual ~Deck();
+  void populate();
+  void shuffle();
+  void deal( Hand );
+  void additionalCards( GenericPlayer& );
+};
+
 #endif
---
+++ source/deck.cpp
@@ -0,0 +1,52 @@
+#include "../headers/deck.h"
+
+#include <iostream>
+#include <algorithm>
+#include "../headers/card.h"
+
+Deck::Deck() {
+  cards.reserve(52);
+  populate();
+}
+
+Deck::~Deck() {
+}
+
+void Deck::populate() {
+  clearHand();
+  // iterate through suits
+  for ( int i = 0; i < 4; i++ ) {
+    Card::SUIT suit = static_cast<Card::SUIT>(i);
+
+    // iterate through ranks
+    for ( int j = 0; i < 13; i++ ) {
+      Card::RANK rank = static_cast<Card::RANK>(j);
+      add( new Card( suit, rank, false ) );
+    }
+  }
+}
+
+void Deck::shuffle() {
+  std::random_shuffle(cards.begin(), cards.end());
+}
+
+void Deck::deal( Hand hand ) {
+  if ( !cards.empty() ) {
+    hand.add( cards.back() );
+    cards.pop_back();
+  } else {
+    std::cout << "Looks like there are no more cards left! (Cannot deal anymore)" << std::endl;
+  }
+}
+
+void Deck::additionalCards( GenericPlayer& gp ) {
+  while ( !gp.isBusted() && gp.isHitting() ) {
+    deal( gp );
+    std::cout << gp << std::endl;
+
+    if ( gp.isBusted() ) 
+      gp.bust();
+  }
+
+};
+
